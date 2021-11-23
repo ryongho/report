c0a0910f9edaf42ee6d1e90bc49146085afefb12
yongho0720 <roh.yh0720@gmail.com>
2021-11-23 21:16:06 +0900

    디자인 수정

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,18 +1,24 @@
 <html>
     <head>
         <title>종목별 호가정보</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <body>
-        <table>
-            <th>시간</th>
-            <th>종목</th>
-            <th>매도 합계</th>            
-            <th>매수 합계</th>
-            <th>메리트</th>
-            <th>가격</th>
-            <th>등락 가격 </th>
-            <th>현재 등락률</th>
-            <th>시간</th>
+        <table class="table table-striped table-sm">
+            <tr>    
+                <th scope="col">시간</th>
+                <th scope="col">종목</th>
+                <th scope="col">매도 합계</th>            
+                <th scope="col">매수 합계</th>
+                <th scope="col">메리트</th>
+                <th scope="col">가격</th>
+                <th scope="col">등락 가격 </th>
+                <th scope="col">현재 등락률</th>
+            </tr>
+            
             @foreach ($rows as $row)
                 <tr>
                     <td>{{ $row->created_at }}</td>
@@ -23,7 +29,6 @@
                     <td>{{ $row->price }}</td>
                     <td>{{ $row->cur_price }}</td>
                     <td>{{ $row->cur_percent }}</td>
-                    <td>{{ $row->created_at }}</td>
                 </tr>
             @endforeach
         </table>
--- resources/views/report_list.blade.php
+++ resources/views/report_list.blade.php
@@ -1,24 +1,21 @@
 <html>
     <head>
         <title>리포트 리스트</title>
-        <style>
-            .list td{
-                border-bottom:1px solid black;
-                border-right:1px solid black;
-            }
-        </style>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <body>
-        <table class="list">
-            <th>종목(코드명)</th>     
-            <th>제목</th>
-            <th>목표가</th>
-            <th>요약</th>
-            <th>추천업체</th>
-            <th>리포트 링크</th>
-            <th>의견</th>
-            <th>작성일</th>
-            <th>수집일</th>
+        <table class="table table-striped table-sm">
+            <th scope="col">종목(코드명)</th>     
+            <th scope="col">제목</th>
+            <th scope="col">목표가</th>
+            <th scope="col" style="width:50%;">요약</th>
+            <th scope="col">추천업체</th>
+            <th scope="col">의견</th>
+            <th scope="col">작성일</th>
+            <th scope="col">수집일</th>
 
             
             @foreach ($reports as $row)
@@ -26,9 +23,8 @@
                     <td>{{ $row->item }} ({{ $row->item_code }})</td>
                     <td><a href={{ $row->report_url }}>{{ $row->title }}</a></td>
                     <td>{{ $row->target_price }}</td>
-                    <td>{{ $row->summary }}</td>
+                    <td style="padding:10;">{{ $row->summary }}</td>
                     <td>{{ $row->company }}</td>
-                    <td><a href={{ $row->report_url }}>리포트 보러가기</a></td>
                     <td>{{ $row->comment }}</td>
                     <td>{{ $row->reg_date }}</td>
                     <td>{{ $row->collect_date }}</td>
